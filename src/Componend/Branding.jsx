9f2585e8913df767cc5e23eedeeb7e4682b27e51
Mahmud hasan <mahmudtarak24@gmail.com>
2024-04-03 01:20:24 +0600

    responsive solve

--- src/Componend/Branding.jsx
+++ src/Componend/Branding.jsx
@@ -6,7 +6,7 @@ import brandinglogo4 from "../assets/brandinglogo4.png";
 const Branding = () => {
   return (
     <div className="container">
-      <div className="md:flex md:py-[116px] sm:pl-[203px] py-[20px] md:pl-0 pl-[60px] items-center gap-[77px] justify-center">
+      <div className="md:flex md:py-[116px] sm:pl-[160px] py-[20px] md:pl-0 pl-[60px] items-center gap-[65px] justify-center">
         <img src={brandinglogo1} alt="" />
         <img src={brandinglogo2} alt="" />
         <img src={brandinglogo3} alt="" />
--- src/Componend/Company.jsx
+++ src/Componend/Company.jsx
@@ -1,19 +1,14 @@
 const Company = () => {
   return (
-    <div className="bg-[#F0F0F0] xl:py-[136px] xl:px-[391px] xl:mt-0 mt-0.25">
+    <div className="bg-[#F0F0F0]">
       <div className="container">
-        <div className="xl:flex">
-          <div className="bg-Primary pt-[100px] pb-[99px] pl-[16%] xl:pl-[74px] sm:pl-[185px] pr-[77px] text-white">
-            <h2 className="text-[36px] font-bold font-popping w-[262px]">
-              Learn more about our company
-            </h2>
-          </div>
-          <div className="bg-company bg-cover bg-no-repeat bg-center w-[100%] flex justify-center">
-            <div className="pt-[168px] pb-[142px]">
-              <a
-                href=""
-                className="py-[14px] px-[30px] bg-white text-Primary text-base font-popping font-semibold"
-              >
+        <div className="md:flex items-center justify-center py-[100px] px-[70px]">
+          <p className="w-[35%] bg-Primary text-white md:text-[36px] text-[20px] font-bold font-popping md:px-[70px] sm:px-[141px] md:py-[50px] sm:py-[50px]">
+            Learn more about our company
+          </p>
+          <div className="bg-company object-cover bg-no-repeat w-[65%]">
+            <div className="flex items-center justify-center py-[105px]">
+              <a className="py-[14px] px-[30px] bg-white text-Primary hover:bg-Primary hover:text-white duration-300 cursor-pointer text-base font-popping font-semibold">
                 Learn More
               </a>
             </div>
